62fa9536ebb6f77416184a2425991ba9081e0dcb
Chris Commisso <qwertykeebored@live.com>
2018-10-18 17:33:04 -0400

    ttt

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<VideoPlayer>().clip = Resources.Load<VideoClip>("Videos/720360");
+       // GetComponent<VideoPlayer>().source = Resources.Load<VideoClip>("Videos/Sonic Animation - SONIC THE HEDGEHOG BATTLE 360° VR- SFM Animation (Sonic Animation)");
+        GetComponent<VideoPlayer>().Prepare();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<VideoPlayer>().isPrepared&&! GetComponent<VideoPlayer>().isPlaying)
+        {
+            GetComponent<VideoPlayer>().Play();
+        }
+	}
+}
